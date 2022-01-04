2be128124c9262ee70203f4c93246db7e79c5363
Nguyễn Tuân <nguyentuan@Nguyens-MacBook-Pro.local>
2022-01-04 10:28:41 +0700

    hgdsf

--- src/routes/about.js
+++ src/routes/about.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const aboutController = require('../app/controllers/AboutController');
 
-            router.use('/:slug', aboutController.show);
+router.use('/:slug', aboutController.show);
 
 router.use('/', aboutController.index);
 
